26bd42669bfecb685ae8b67b3b08fdd67852c9c3
Paritosh Tripathi <76477608+paritoshtripathi935@users.noreply.github.com>
2021-11-28 15:49:46 +0530

    Add files via upload

---
+++ Dsa/complexity_analysis/TEST.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	while(1){
+        int x;
+        cin>>x;
+        if(x==42){
+            break;
+        }
+        else{
+            cout<<x<<endl;
+        }
+    }
+    return 0;
+}
