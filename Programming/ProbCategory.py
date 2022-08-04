b6700c28d3402486c7a9c742a2a1264d1904e246
Paritosh Tripathi <tripathiparitosh935@gmail.com>
2022-08-04 21:39:20 +0530

    added ProbCategory.py

--- Programming/ProbCategory.py
+++ Programming/ProbCategory.py
@@ -3,10 +3,10 @@ def ProbCategory():
      for i in range(T):
         diff = int(input())
         if diff >= 1 and diff < 100:
-            print("Easy", diff)
+            print("Easy")
         if diff >= 100 and diff < 200:
             print("Medium")
-        if diff >= 200 and diff < 300:
+        if diff >= 200 and diff <= 300:
             print("Hard")
 
 ProbCategory()
