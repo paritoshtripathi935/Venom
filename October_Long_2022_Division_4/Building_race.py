3ed46dd1220b8b031616472282124ecea8d1cffb
Paritosh <tripathiparitosh935@gmail.com>
2022-10-08 18:08:07 +0530

    added Building_race.py

--- October_Long_2022_Division_4/Building_race.py
+++ October_Long_2022_Division_4/Building_race.py
@@ -14,27 +14,20 @@ def BR():
         #print("Constraints not satisfied")
         return
     for i in range(t):
-        a, b, x, y = map(int, input().split())
+        a, b, x, y = map(float, input().split())
         # check for constraints
         if a < 1 or a > 100 or b < 1 or b > 100 or x < 1 or x > 10 or y < 1 or y > 10:
             #print("Constraints not satisfied")
             return
-        if a == b:
-            print("Both")
-        elif a > b:
-            if x > y:
-                print("Chef")
-            elif x < y:
-                print("Chefina")
-            else:
-                print("Both")
+        
+        chef_time = a/x
+        chefina_time = b/y
+        if chef_time < chefina_time:
+            print("Chef")
+        elif chef_time > chefina_time:
+            print("Chefina")
         else:
-            if x > y:
-                print("Chefina")
-            elif x < y:
-                print("Chef")
-            else:
-                print("Both")
+            print("Both")
 
 if __name__ == "__main__":
     BR()
