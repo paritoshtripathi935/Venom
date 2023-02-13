ea7bf7af47f2fddd761d4df6a3bdce46ca109298
Paritosh Tripathi <tripathiparitosh935@gmail.com>
2023-02-13 18:56:20 +0530

    Leetcode Day 2 - Question 1

--- Dsa/Arrays/twoSum.py
+++ Dsa/Arrays/twoSum.py
@@ -4,3 +4,13 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return i, j
+                
+# optimized solution for above code
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums): # enumerate returns index and value of list
+            if target - num in seen: # if target - num is in seen dictionary then
+                return [seen[target - num], i]
+            seen[num] = i
